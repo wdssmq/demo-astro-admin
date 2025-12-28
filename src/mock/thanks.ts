ddf46b9a19fbc9f41025a98ea60efb59f4e7e1f8
wdssmq <wdssmq@qq.com>
2025-12-28 14:21:20 +0800

    feat: add backend layout and new article management pages
    - Created `article.astro` for article management with a backend layout.
    - Updated `index.astro` to include a dashboard with various statistics and a backend layout.
    - Added `write.astro` for creating new articles with a backend layout.
    - Introduced a new `style.less` file for styling the backend interface, including responsive design and dark mode support.

---
+++ src/mock/thanks.ts
@@ -0,0 +1,222 @@
+const thanks = {
+  "id": "thankslist",
+  "title": "Z-BlogPHP网站和程序开发",
+  "sections": [
+    {
+      "category": "程序",
+      "icon": "icon-gear-wide-connected",
+      "items": [
+        {
+          "name": "zx.asd",
+          "url": "https://bbs.zblogcn.com/user-zx.asd.html",
+          "title": "宇宙飞天神猪！！！",
+        },
+        {
+          "name": "zsx",
+          "url": "https://www.zsxsoft.com",
+          "title": "路过的",
+          "target": "_blank"
+        },
+        {
+          "name": "未寒",
+          "url": "https://imzhou.com",
+          "title": "毁人不倦！",
+        },
+        {
+          "name": "zhouzishu",
+          "url": "https://zzs.im/",
+        }
+      ]
+    },
+    {
+      "category": "协作",
+      "icon": "icon-x-diamond-fill",
+      "items": [
+        {
+          "name": "ice",
+          "url": "https://iceui.cn",
+          "title": "BT编程语言创始人",
+        },
+        {
+          "name": "[BT语言]",
+          "url": "https://btlang.org/",
+        },
+        {
+          "name": "沉水浮冰",
+          "url": "https://www.wdssmq.com/",
+        },
+        {
+          "name": "橙色阳光",
+          "url": "https://www.inull.cn",
+        },
+        {
+          "name": "花晨月夕",
+          "url": "https://kfuu.cn",
+          "title": "可风网",
+        },
+        {
+          "name": "唐朝",
+          "url": "http://www.ytecn.com",
+        },
+        {
+          "name": "心扬",
+          "url": "https://app.zblogcn.com/?auth=2ffbff0a-1207-4362-89fb-d9a780125e0a",
+        },
+        {
+          "name": "Lucky",
+          "url": "https://www.songhaifeng.com/",
+        }
+      ]
+    },
+    {
+      "category": "界面",
+      "icon": "icon-image-fill",
+      "items": [
+        {
+          "name": "拓源",
+          "url": "https://www.toyean.com",
+        },
+        {
+          "name": "吉光片羽",
+          "url": "https://www.jgpy.cn",
+          "title": "他太轻了,就像一片羽毛,风一吹就飞了.为了避免乱飞,他的内心就渐渐纠结起来了.",
+        },
+        {
+          "name": "娇娇",
+          "title": "美丽又婉约的湘妹子,可爱的外表下掩藏着古典美的气质,Z-Blog官网的设计者,现在深圳做着喜欢的工作",
+          "type": "span"
+        },
+        {
+          "name": "新鲜人",
+          "url": "http://www.htmlit.com.cn",
+        }
+      ]
+    },
+    {
+      "category": "支持",
+      "icon": "icon-lightning-fill",
+      "items": [
+        {
+          "name": "Haphic",
+          "url": "http://www.esloy.com",
+        },
+        {
+          "name": "月上之木",
+          "url": "https://bbs.zblogcn.com/user-%E6%9C%88%E4%B8%8A%E4%B9%8B%E6%9C%A8.html",
+        },
+        {
+          "name": "Washun",
+          "url": "http://www.washun.com",
+        },
+        {
+          "name": "小飞龙",
+          "url": "https://bbs.zblogcn.com/user-%E5%B0%8F%E9%A3%9E%E9%BE%99.html",
+        },
+        {
+          "name": "拾叁",
+          "url": "https://bbs.zblogcn.com/user-%E6%8B%BE%E5%8F%81.html",
+        },
+        {
+          "name": "Captian",
+          "url": "http://www.nicwind.com",
+        },
+        {
+          "name": "天兴",
+          "url": "https://www.txcstx.cn/",
+        }
+      ]
+    },
+    {
+      "category": "感谢",
+      "icon": "icon-peace-fill",
+      "items": [
+        {
+          "name": "空城",
+          "title": "空大师乃是PHP高手,ZBP的指引者!",
+          "type": "span"
+        },
+        {
+          "name": "楊懿軒",
+          "title": "為天地立心，為生民立命，為往聖繼絕學，為萬世開太平。",
+          "type": "span"
+        },
+        {
+          "name": "龙威廉",
+          "url": "http://www.williamlong.info",
+        },
+        {
+          "name": "卢松松",
+          "url": "http://lusongsong.com",
+        },
+        {
+          "name": "瑜廷",
+          "url": "https://bbs.zblogcn.com/user-ywte2000.html",
+        },
+        {
+          "name": "流年",
+          "url": "https://www.huisem.com",
+        },
+        {
+          "name": "尔今",
+          "url": "https://www.yiwuku.com/",
+        },
+        {
+          "name": "墨初",
+          "url": "https://www.73so.com/",
+        },
+        {
+          "name": "烽烟",
+          "url": "https://www.fengyan.cc/",
+        },
+        {
+          "name": "老李",
+          "url": "https://yeelz.com/",
+        },
+        {
+          "name": "大河",
+          "url": "https://www.iddahe.com/",
+        },
+        {
+          "name": "老张",
+          "url": "https://www.jstang.cn/",
+        },
+        {
+          "note": "等朋友，还有您..."
+        }
+      ]
+    },
+    {
+      "category": "链接",
+      "icon": "icon-geo-alt-fill",
+      "items": [
+        {
+          "name": "GitHub",
+          "url": "https://github.com/zblogcn/zblogphp",
+          "title": "Z-BlogPHP on GitHub",
+        },
+        {
+          "name": "程序下载",
+          "url": "https://www.zblogcn.com/zblogphp",
+        },
+        {
+          "name": "应用中心",
+          "url": "https://app.zblogcn.com",
+        },
+        {
+          "name": "文档Docs",
+          "url": "https://docs.zblogcn.com",
+        },
+        {
+          "name": "ZBlogger社区",
+          "url": "https://bbs.zblogcn.com",
+        },
+        {
+          "name": "Z5加密",
+          "url": "https://z5encrypt.com",
+        }
+      ]
+    }
+  ]
+};
+
+export default thanks;
